7bce7f80a09ecc42d9f609c1ab270326ba2bea8f
SignpostMarv <github@signpostmarv.name>
2012-05-20 20:05:07 +0100

    updating documentation

--- docs/Aurora-Sim/d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.js
+++ docs/Aurora-Sim/d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.js
@@ -3,6 +3,8 @@ var interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule =
     [ "GetRegionFlags", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#adaa12d0a02570ea3904b381e6438e37c", null ],
     [ "IsManager", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#ada95df26a7b3142ad030db89e020c6aa", null ],
     [ "sendRegionHandshakeToAll", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#acc79c8cf45536026761a73cb2467c4a7", null ],
+    [ "setEstateTerrainBaseTexture", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#a8e59a50a0b3c1c8311aa14548d7a8b6f", null ],
+    [ "setEstateTerrainTextureHeights", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#afebd7b5d313680e0aa59dae2f6a1b35b", null ],
     [ "SetSceneCoreDebug", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#a93e741fd4e0e81ed926eb8be5e73d0db", null ],
     [ "TriggerEstateSunUpdate", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#a9287b363f0b7557beb3e300004d449db", null ],
     [ "OnEstateInfoChange", "d8/d9f/interfaceOpenSim_1_1Region_1_1Framework_1_1Interfaces_1_1IEstateModule.html#aa13d1b4947d0f8db4f4555c416d051a1", null ],
--- docs/Aurora-Sim/search/enums_74.js
+++ docs/Aurora-Sim/search/enums_74.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['threatlevel',['ThreatLevel',['../d9/deb/namespaceAurora_1_1ScriptEngine_1_1AuroraDotNetEngine.html#a64a4fe0ff6addc76451284cc0987fca2',1,'Aurora.ScriptEngine.AuroraDotNetEngine.ThreatLevel()'],['../d7/d41/namespaceOpenSim_1_1Services_1_1Interfaces.html#a976e27ab49e2b4084ad813ed7451468d',1,'OpenSim.Services.Interfaces.ThreatLevel()']]]
+  ['threatlevel',['ThreatLevel',['../d9/deb/namespaceAurora_1_1ScriptEngine_1_1AuroraDotNetEngine.html#a64a4fe0ff6addc76451284cc0987fca2',1,'Aurora.ScriptEngine.AuroraDotNetEngine.ThreatLevel()'],['../d9/deb/namespaceAurora_1_1ScriptEngine_1_1AuroraDotNetEngine.html#a64a4fe0ff6addc76451284cc0987fca2',1,'Aurora.ScriptEngine.AuroraDotNetEngine.ThreatLevel()'],['../d7/d41/namespaceOpenSim_1_1Services_1_1Interfaces.html#a976e27ab49e2b4084ad813ed7451468d',1,'OpenSim.Services.Interfaces.ThreatLevel()']]]
 ];
